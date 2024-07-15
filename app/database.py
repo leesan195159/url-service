0c54f7d28df2032a5a1aeb6d41e6f44cdec0720d
leesan195159 <leesan195159@users.noreply.github.com>
2024-07-15 17:33:06 +0900

    test 코드 작성

--- app/database.py
+++ app/database.py
@@ -1,7 +1,7 @@
 import redis
+import sqlalchemy
 
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 redis_client = redis.StrictRedis(host='localhost', port=6379, db=0)
@@ -9,7 +9,7 @@ database_url = "mysql+pymysql://ls:1234@localhost/url_service"
 
 engine = create_engine(database_url)
 session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-base = declarative_base()
+base = sqlalchemy.orm.declarative_base()
 
 
 def get_db():
