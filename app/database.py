de2563eef7b306a2ba3a78f8885e0851530a881a
leesan195159 <leesan195159@users.noreply.github.com>
2024-07-15 11:54:33 +0900

    통계 기능 구현

--- app/database.py
+++ app/database.py
@@ -1,7 +1,10 @@
+import redis
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+redis_client = redis.StrictRedis(host='localhost', port=6379, db=0)
 database_url = "mysql+pymysql://ls:1234@localhost/url_service"
 
 engine = create_engine(database_url)
